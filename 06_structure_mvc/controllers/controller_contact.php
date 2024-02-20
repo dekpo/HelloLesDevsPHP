59ee949f9a2fed7940a64e817cd3db12ff6b4897
Dekpo-Wyna Yologaza <dekpo@me.com>
2024-02-20 10:23:52 +0100

    Added little logic to load controllers and display views

---
+++ 06_structure_mvc/controllers/controller_contact.php
@@ -0,0 +1,5 @@
+<?php
+// ma logique de controller
+// blah blah blah
+// On charge la vue
+include "./views/base.phtml";
--- 06_structure_mvc/index.php
+++ 06_structure_mvc/index.php
@@ -1,11 +1,11 @@
 <?php
 // Si la variable page est définié dans l'url
-// On la récupère sinon c'est "home"
-$getPage = isset($_GET['page']) ? $_GET['page'] : "home";
-// On définit le parcours pour charger la page souhaitée
+// On la récupère sinon c'est "vide"
+$getPage = isset($_GET['page']) ? strtolower($_GET['page']) : "";
+// On définit le parcours pour charger le controller souhaité
 $path = "./controllers/controller_" . $getPage . ".php";
 // Si le fichier n'existe pas le chemin n'est pas correct
 // on a un parcours vers home qui évite d'afficher une erreur
-$controller = file_exists($path) ? $path : "./controllers/controller_home.php";
-// On charge la page souhaitée avec un include ou un require
-require $controller;
+$page = file_exists($path) ? $getPage : "home";
+// On charge le controller souhaité avec un include ou un require
+require "./controllers/controller_{$page}.php";
