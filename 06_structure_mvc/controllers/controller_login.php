e5cce6add304d550a5d1369a053859a4d0482266
Dekpo-Wyna Yologaza <dekpo@me.com>
2024-02-22 13:53:25 +0100

    Added login errors logic and template

--- 06_structure_mvc/controllers/controller_login.php
+++ 06_structure_mvc/controllers/controller_login.php
@@ -1,7 +1,4 @@
 <?php
-// echo "<pre>";
-// var_dump($_POST);
-// echo "</pre>";
 // SI LE FORMULAIRE EST VALIDE
 if (isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['email']) && !empty($_POST['password'])) {
     $password = htmlentities(strip_tags($_POST['password']));
@@ -9,22 +6,26 @@ if (isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['email'
     // On se connecte à la base de données 
     // Pour vérifier si l'email est dans la table user si c'est le cas on récupère les datas de l'utilisateur
     $db = connectDB();
-    $query = $db->prepare("SELECT * FROM user WHERE email=:email");
+    $query = $db->prepare("SELECT * FROM user WHERE email=:email LIMIT 1");
     // bindParam permet de renseigner la requête afin de "protéger" le serveur SQL
     $query->bindParam(':email', $email);
     $query->execute();
+    // On stocke toutes les infos user dans la variable $user
     $user = $query->fetch(PDO::FETCH_ASSOC);
-    // echo "<pre>";
-    // var_dump($user);
-    // echo "</pre>";
-
-    if (is_array($user)) {
-        $db_hash = $user['password'];
-        if (password_verify($password, $db_hash)) {
-            echo "YES !";
-        } else {
-        echo "INVALID ;(";
-        }
+    // Au départ on suppose qu'il n'y a pas d'erreur
+    // Le tableau des erreurs est donc vide
+    $errors = [];
+    // Si la variable $user n'est pas un tableau
+    // Le compte n'est pas présent dans la base de données => error
+    if (!is_array($user)){
+        $errors[] = "Le compte $email n'existe pas. Veuillez <a href=\"?page=contact\">vous inscrire</a> svp.";
+    }
+    // Le compte est présent dans la BDD mais le mot-de-passe ne match pas => error
+    if (is_array($user) && !password_verify($password, $user['password'])) {
+        $errors[] = "Le mot-de-passe semble incorrect.";
+    }
+    if (empty($errors)){
+        header("Location:?page=home");
     }
 }
 // ma logique de controller
