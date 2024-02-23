39a1fe2aaecd74ce84bad1c276d79eeb709d6a2e
Dekpo-Wyna Yologaza <dekpo@me.com>
2024-02-23 15:05:28 +0100

    Added controller and template for admin post add

---
+++ 06_structure_mvc/controllers/controller_admin_post_add.php
@@ -0,0 +1,14 @@
+<?php
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+// ma logique de controller
+// SI LE FORMULAIRE EST VALIDE
+
+// On récup les champs du formulaire et on effectue une requête d'insertion
+
+// Après l'insertion on redirige l'utilisateur sur le dashboard admin
+
+// blah blah blah
+// On charge la vue
+include "./views/base.phtml";
