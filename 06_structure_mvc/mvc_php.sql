59a10251c93e9926fa7e7e22cf48feb438346279
Dekpo-Wyna Yologaza <dekpo@me.com>
2024-02-26 12:09:38 +0100

    Updated SQL file for training

--- 06_structure_mvc/mvc_php.sql
+++ 06_structure_mvc/mvc_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : ven. 23 fév. 2024 à 15:47
+-- Généré le : lun. 26 fév. 2024 à 11:08
 -- Version du serveur : 11.2.2-MariaDB-1:11.2.2+maria~ubu2204
 -- Version de PHP : 8.2.9
 
@@ -46,7 +46,7 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id`, `user_id`, `firstname`, `lastname`, `address1`, `address2`, `city`, `state`, `zip`, `message`, `created_at`) VALUES
-(1, 5, 'Dekpo Wyna', 'Yologaza', '2 Rue du Lavoir', '', '74150 - RUMILLY', 'Auvergne-Rh&ocirc;ne-Alpes', '74150', 'Hello les devs PHP !!!', '2024-02-21 14:29:45'),
+(1, 5, 'Charles', 'Beaudel\'aire', '2 Rue du Lavoir', '', '74150 - RUMILLY', 'Auvergne-Rh&ocirc;ne-Alpes', '74150', 'Hello les devs PHP !!!', '2024-02-21 14:29:45'),
 (2, 6, 'Dekpo Wyna', 'Yologaza', '2 Rue du Lavoir', '', '74150 - RUMILLY', 'Choose...', '74150', '', '2024-02-21 15:21:08'),
 (3, 7, 'Dekpo Gmail', 'Yologaza', '2 Rue du Lavoir', '', 'RUMILLY', 'Corse', '74150', '', '2024-02-21 15:35:16'),
 (4, 8, 'Dekpo Wyna', 'Yologaza', '2 Rue du Lavoir', '', '74150 - RUMILLY', 'Auvergne-Rh&ocirc;ne-Alpes', '74150', '', '2024-02-22 08:18:13');
@@ -59,6 +59,7 @@ INSERT INTO `contact` (`id`, `user_id`, `firstname`, `lastname`, `address1`, `ad
 
 CREATE TABLE `post` (
   `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT 5,
   `title` varchar(80) NOT NULL,
   `description` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
@@ -69,24 +70,27 @@ CREATE TABLE `post` (
 -- Déchargement des données de la table `post`
 --
 
-INSERT INTO `post` (`id`, `title`, `description`, `image`, `created_at`) VALUES
-(1, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. ', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
-(2, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
-(5, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
-(6, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
-(7, 'Mon troisième article de blog ehehe', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2015/01/28/23/34/mountains-615428_1280.jpg', '2024-02-20 10:26:38'),
-(8, 'Quatrième article de blog ouhouhou', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg', '2024-02-20 10:29:49'),
-(9, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
-(10, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
-(11, 'Mon troisième article de blog ehehe', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2015/01/28/23/34/mountains-615428_1280.jpg', '2024-02-20 10:26:38'),
-(12, 'Quatrième article de blog ouhouhou', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg', '2024-02-20 10:29:49'),
-(13, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
-(14, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
-(15, 'Mon troisième article de blog ehehe', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2015/01/28/23/34/mountains-615428_1280.jpg', '2024-02-20 10:26:38'),
-(16, 'Quatrième article de blog ouhouhou', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg', '2024-02-20 10:29:49'),
-(17, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
-(18, 'Mon deuxi&egrave;me article de blog eheheh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
-(21, 'Titre de mon article hello', 'Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah hello', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-23 14:18:45');
+INSERT INTO `post` (`id`, `user_id`, `title`, `description`, `image`, `created_at`) VALUES
+(1, 5, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. ', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
+(2, 5, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
+(5, 5, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
+(6, 5, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
+(7, 5, 'Mon troisième article de blog ehehe', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2015/01/28/23/34/mountains-615428_1280.jpg', '2024-02-20 10:26:38'),
+(8, 5, 'Quatrième article de blog ouhouhou', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg', '2024-02-20 10:29:49'),
+(9, 5, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
+(10, 5, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
+(11, 5, 'Mon troisième article de blog ehehe', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2015/01/28/23/34/mountains-615428_1280.jpg', '2024-02-20 10:26:38'),
+(12, 5, 'Quatrième article de blog ouhouhou', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg', '2024-02-20 10:29:49'),
+(13, 5, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
+(14, 5, 'Mon deuxième article de blog ohoh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
+(15, 5, 'Mon troisième article de blog ehehe', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2015/01/28/23/34/mountains-615428_1280.jpg', '2024-02-20 10:26:38'),
+(16, 5, 'Quatrième article de blog ouhouhou', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg', '2024-02-20 10:29:49'),
+(17, 5, 'Mon premier article de blog', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-20 10:24:09'),
+(18, 5, 'Mon deuxi&egrave;me article de blog eheheh', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vel exercitationem quasi, nemo voluptatem eligendi neque, repellendus unde iusto explicabo numquam voluptatum illum expedita ab quia, quibusdam doloribus dolorum. Nulla.', 'https://cdn.pixabay.com/photo/2013/08/28/00/54/field-176602_1280.jpg', '2024-02-20 10:26:38'),
+(21, 5, 'Titre de mon article hello', 'Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah Blabh blah hello', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-23 14:18:45'),
+(22, 5, 'Mon nouveau post du jour', 'Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah Blah blah ', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-26 08:55:51'),
+(23, 5, 'Hello test', 'Hello hello Hello hello Hello hello Hello hello Hello hello Hello hello Hello hello ', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-26 09:12:20'),
+(24, 6, 'Nouveau post avec mon compte Gmail', 'Nouveau post avec mon compte Gmail Nouveau post avec mon compte GmailNouveau post avec mon compte GmailNouveau post avec mon compte GmailNouveau post avec mon compte GmailNouveau post avec mon compte Gmail', 'https://cdn.pixabay.com/photo/2024/01/11/09/50/village-8501168_1280.jpg', '2024-02-26 09:14:13');
 
 -- --------------------------------------------------------
 
@@ -108,7 +112,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `email`, `password`, `roles`, `registered_at`) VALUES
 (5, 'dekpo@me.com', '$2y$10$m8a9JOxwYncQaN1pP126MemmU2Uuh4U2E4F5laI3.d/3pC6WaghT2', '[\"ROLE_ADMIN\",\"ROLE_MEMBER\"]', '2024-02-21 14:29:45'),
-(6, 'dw.yologaza@gmail.com', '$2y$10$5XqlJxiRx6wVLBu3YXZRIuRLAO9t6E.sms9WOJNP7z1c6fop5LkIe', '[]', '2024-02-21 15:21:08'),
+(6, 'dw.yologaza@gmail.com', '$2y$10$m8a9JOxwYncQaN1pP126MemmU2Uuh4U2E4F5laI3.d/3pC6WaghT2', '[\"ROLE_ADMIN\",\"ROLE_MEMBER\"]', '2024-02-21 15:21:08'),
 (7, 'dw.yologaza@wanadoo.fr', '$2y$10$xIxrwv4.SN6HCZVDYZT95.Q/1uUrf.PZCZslGSH6aAM0DBKGxLu8y', '[]', '2024-02-21 15:35:16'),
 (8, 'dekpo@icloud.com', '$2y$10$m8a9JOxwYncQaN1pP126MemmU2Uuh4U2E4F5laI3.d/3pC6WaghT2', '[]', '2024-02-22 08:18:13');
 
@@ -127,7 +131,8 @@ ALTER TABLE `contact`
 -- Index pour la table `post`
 --
 ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_post_user` (`user_id`);
 
 --
 -- Index pour la table `user`
@@ -149,7 +154,7 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT pour la table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT pour la table `user`
@@ -166,6 +171,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `contact`
   ADD CONSTRAINT `fk_contact_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Contraintes pour la table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `fk_post_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
