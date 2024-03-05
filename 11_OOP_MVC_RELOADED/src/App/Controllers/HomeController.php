273536dc80779fe3b2a06f1b1c360ecc39f9e00c
Dekpo-Wyna Yologaza <dekpo@me.com>
2024-03-05 09:19:14 +0100

    Made a PostManager to handle queries

--- 11_OOP_MVC_RELOADED/src/App/Controllers/HomeController.php
+++ 11_OOP_MVC_RELOADED/src/App/Controllers/HomeController.php
@@ -2,13 +2,13 @@
 namespace App\Controllers;
 
 use App\Controllers\AbstractController;
-use App\Models\Post;
+use App\Models\PostManager;
 
 class HomeController extends AbstractController{
     public function index(){
         $title = "Hello OOP World";
         $template = './views/template_home.phtml';
-        $p = new Post();
+        $p = new PostManager();
         $posts = $p->getAll(3);
         $this->render($template,[
             'title'=>$title,
